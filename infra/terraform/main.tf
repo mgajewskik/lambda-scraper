157a8623e87118c3ed2fddbe52767500913f4ad6
mgajewskik <mgajewskik@gmail.com>
2022-07-31 14:17:04 +0200

    Add public access block to S3 bucket

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -40,6 +40,16 @@ locals {
 
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "${local.lambda_name}-${random_uuid.bucket.id}"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
